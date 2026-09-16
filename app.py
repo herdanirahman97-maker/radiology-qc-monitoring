04596315bb9d29ecd0f85e9f00791406853298f1
herdanirahman97-maker <herdani.rahman97@gmail.com>
2026-09-16 14:53:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def build_room_html(sheet_name, file_name, df, logo_data_uri):
     
     html += f"""
         </table>
-        <div class="trademark">Source & maintained by Herdani Rahman</div>
+        <div class="trademark">Source & maintained by Herdani Rahman Account</div>
     </div>
     """
     return html
@@ -311,7 +311,10 @@ def build_qc_html(sheet_name, file_name, df, logo_data_uri, sig1_uri, sig2_uri, 
             </tr>
         </table>
     </div>
-    <div class="trademark">Source & maintained by Herdani Rahman</div>
+    <div class="trademark">Source & maintained by Herdani Rahman Account
+    
+    
+    </div>
     </div>
     """
     return html
